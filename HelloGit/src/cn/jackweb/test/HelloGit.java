d1e45631ae8a667c61638610e149158760351919
Jack Chan <jkxl2018@gmail.com>
2019-04-03 10:52:22 +0800

    This is my first git project on IDEA,maybe just a test project, but I think very happy!

---
+++ HelloGit/src/cn/jackweb/test/HelloGit.java
@@ -0,0 +1,14 @@
+package cn.jackweb.test;
+
+/**
+ * @author Jack Chan
+ * @date 2019-04-03 10:47
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git, You are on IDEA?");
+
+        System.out.println("Wow! So wonderful!");
+
+    }
+}
